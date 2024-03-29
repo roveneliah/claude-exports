d0560c3c406fabc3b5a0a0af5770e90b150d2cfa
Eli <ehanover21@gmail.com>
2024-03-29 10:27:10 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,46 +1,5 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   var exportButton = document.getElementById('exportButton');
-//   exportButton.addEventListener('click', exportChat);
-// });
-
-// function exportChat() {
-//   console.log('Sending message to content script');
-//   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-//     chrome.scripting.executeScript({
-//       target: { tabId: tabs[0].id },
-//       files: ['content.js']
-//     }, function() {
-//       chrome.tabs.sendMessage(tabs[0].id, { action: 'exportChat' }, function(response) {
-//         if (chrome.runtime.lastError) {
-//           console.error('Error: ', chrome.runtime.lastError.message);
-//         } else {
-//           if (response && response.chatContent) {
-//             console.log(response)
-//             console.log(response.chatContent)
-//             var chatContent = response.chatContent;
-//             saveToObsidian(chatContent);
-//           }
-//         }
-//       });
-//     });
-//   });
-// }
-
-// function saveToObsidian(content) {
-//   var timestamp = new Date().toISOString().replace(/[:T]/g, '-').slice(0, -5);
-//   var filename = `chat-log-${timestamp}.md`;
-//   var blob = new Blob([content], { type: 'text/markdown' });
-//   var url = URL.createObjectURL(blob);
-//   chrome.downloads.download({
-//     url: url,
-//     filename: filename,
-//     saveAs: true
-//   });
-// }
 
 document.addEventListener('DOMContentLoaded', function() {
-  // var exportButton = document.getElementById('exportButton');
-  // exportButton.addEventListener('click', exportChat);
   exportChat();
 });
 
